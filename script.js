7324c8d97e823b9d65dbdb3cfae66c9664dfd2ed
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-06 21:27:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function firstWord(s) {
   // your code here
+	let scount=0;
+	let res='';
+	for(let i=0;i<s.length();i++){
+		if(scount<1){
+			if(s[i]!==' '){
+				res=res+s[i];
+			}
+			else if(s[i]===''){
+				scount++;
+				
+			}
+		}
+		else if{
+			return res;
+		}
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
